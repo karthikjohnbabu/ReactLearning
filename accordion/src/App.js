ef8b0caa0119cc334fb6099ac7d74eed006ca51f
Karthik Babu <karthik-babu@dedagroup.it>
2024-12-18 17:10:19 +0530

    Initial React Project

---
+++ accordion/src/App.js
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import "./style.css";
+
+const faqs = [
+  {
+    title: "Where are these chairs assembled?",
+    text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium, quaerat temporibus quas dolore provident nisi ut aliquid ratione beatae sequi aspernatur veniam repellendus.",
+  },
+  {
+    title: "How long do I have to return my chair?",
+    text: "Pariatur recusandae dignissimos fuga voluptas unde optio nesciunt commodi beatae, explicabo natus.",
+  },
+  {
+    title: "Do you ship to countries outside the EU?",
+    text: "Excepturi velit laborum, perspiciatis nemo perferendis reiciendis aliquam possimus dolor sed! Dolore laborum ducimus veritatis facere molestias!",
+  },
+];
+
+export default function App() {
+  return (
+    <div>
+      <Accordion data={faqs} />
+    </div>
+  );
+}
+
+function Accordion({ data }) {
+  const [curOpen, setIsOpen] = useState(null);
+  return (
+    <div className="accordion">
+      {data.map((el, i) => (
+        <AccordionItem
+          curOpen={curOpen}
+          onOpen={setIsOpen}
+          title={el.title}
+          text={el.text}
+          num={i}
+          key={el.title}
+        />
+      ))}
+    </div>
+  );
+}
+
+function AccordionItem({ num, title, text, curOpen, onOpen }) {
+  return;
+}
